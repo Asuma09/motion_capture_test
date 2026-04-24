ca05bda7b64821c767ac6461c10fcf1a5fa56041
Asuma09 <172264557+Asuma09@users.noreply.github.com>
2026-04-24 14:06:38 +0900

    Initial Commit

---
+++ motion_capture_test/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  motion_capture_test
+//
+//  Created by 飛鳥馬空 on 2026/04/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
